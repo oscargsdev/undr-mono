196dc92aea4e3a49695729e06ac5c445058e56f1
Oscar Garcia <oscargs.dev@gmail.com>
2026-09-09 21:14:50 -0600

    Server setup and health handler

--- cmd/undr/main.go
+++ cmd/undr/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
 	"slices"
 	"strconv"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -36,6 +42,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -55,13 +62,11 @@ func main() {
 		logger: logger,
 	}
 
-	app.logger.Info("starting app",
-		"port", app.config.port,
-		"env", app.config.env,
-		"dsn", app.config.db.dsn,
-		"max-open-conns", app.config.db.maxOpenConns,
-		"max-idle-conns", app.config.db.maxIdleConns,
-		"max-idle-time", app.config.db.maxIdleTime)
+	err = app.serve()
+	if err != nil {
+		app.logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
 
 func loadConfig(cfg *config) {
@@ -130,4 +135,66 @@ func loadConfig(cfg *config) {
 		}
 		cfg.db.maxIdleTime = time.Duration(maxIdleTime) * time.Minute
 	}
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+	}
+
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		app.logger.Info("shutting down server", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+
+		app.logger.Info("completing background tasks", "addr", srv.Addr)
+
+		app.wg.Wait()
+		shutdownError <- nil
+	}()
+
+	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("stopped server", "addr", srv.Addr)
+
+	return nil
+}
+
+func (app *application) routes() http.Handler {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /health/live", live)
+
+	return router
+}
+
+func live(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "alive and rocking")
 }
