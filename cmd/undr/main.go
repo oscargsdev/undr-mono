b6b9bc879415c1e64e249842b6734d6c70e3a874
Oscar Garcia <oscargs.dev@gmail.com>
2026-09-09 20:38:13 -0600

    Function to load config values from env. Default values for config.

--- cmd/undr/main.go
+++ cmd/undr/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"time"
 
 	"github.com/joho/godotenv"
 )
+
+const (
+	DefaultPort         = 4004
+	DefaultEnv          = "devtest"
+	DefaultDSN          = "postgrestest"
+	DefaultMaxOpenConns = 250
+	DefaultMaxIdleConns = 250
+	DefaultMaxIdleTime  = 150
+)
+
+var ValidEnvs = []string{"dev", "staging", "prod"}
 
 type config struct {
 	port int
@@ -28,43 +40,7 @@ func main() {
 	}
 
 	var cfg config
-
-	// TODO: Handle defaults
-	portStr := os.Getenv("PORT")
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		fmt.Printf("error loading server port: %v", err)
-		os.Exit(1)
-	}
-	cfg.port = port
-
-	cfg.env = os.Getenv("ENV")
-
-	cfg.db.dsn = os.Getenv("DSN")
-
-	maxOpenConnsStr := os.Getenv("MAX_OPEN_CONNS")
-	maxOpenConns, err := strconv.Atoi(maxOpenConnsStr)
-	if err != nil {
-		fmt.Printf("error loading max-open-conns: %v", err)
-		os.Exit(1)
-	}
-	cfg.db.maxOpenConns = maxOpenConns
-
-	maxIdleConnsStr := os.Getenv("MAX_IDLE_CONNS")
-	maxIdleConns, err := strconv.Atoi(maxIdleConnsStr)
-	if err != nil {
-		fmt.Printf("error loading max-idle-conns: %v", err)
-		os.Exit(1)
-	}
-	cfg.db.maxIdleConns = maxIdleConns
-
-	maxIdleTimeStr := os.Getenv("MAX_IDLE_TIME")
-	maxIdleTime, err := strconv.Atoi(maxIdleTimeStr)
-	if err != nil {
-		fmt.Printf("error loading max-idle-time: %v", err)
-		os.Exit(1)
-	}
-	cfg.db.maxIdleTime = time.Duration(maxIdleTime) * time.Minute
+	loadConfig(&cfg)
 
 	fmt.Println("port: ", cfg.port)
 	fmt.Println("env: ", cfg.env)
@@ -72,4 +48,73 @@ func main() {
 	fmt.Println("max-open-conns: ", cfg.db.maxOpenConns)
 	fmt.Println("max-idle-conns: ", cfg.db.maxIdleConns)
 	fmt.Println("max-idle-time: ", cfg.db.maxIdleTime)
+
+}
+
+func loadConfig(cfg *config) {
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		cfg.port = DefaultPort
+	} else {
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			fmt.Printf("error loading server port: %v \n", err)
+			os.Exit(1)
+		}
+		cfg.port = port
+	}
+
+	envStr := os.Getenv("ENV")
+	if envStr == "" {
+		cfg.env = DefaultEnv
+	} else {
+		if !slices.Contains(ValidEnvs, envStr) {
+			fmt.Print("error loading environment: invalid value: ", envStr, "\n")
+			os.Exit(1)
+		}
+		cfg.env = envStr
+	}
+
+	dsnStr := os.Getenv("DSN")
+	if dsnStr == "" {
+		cfg.db.dsn = DefaultDSN
+	} else {
+		cfg.db.dsn = os.Getenv("DSN")
+	}
+
+	maxOpenConnsStr := os.Getenv("MAX_OPEN_CONNS")
+	if maxOpenConnsStr == "" {
+		cfg.db.maxOpenConns = DefaultMaxOpenConns
+	} else {
+		maxOpenConns, err := strconv.Atoi(maxOpenConnsStr)
+		if err != nil {
+			fmt.Printf("error loading max open connections: %v \n", err)
+			os.Exit(1)
+		}
+		cfg.db.maxOpenConns = maxOpenConns
+	}
+
+	maxIdleConnsStr := os.Getenv("MAX_IDLE_CONNS")
+	if maxIdleConnsStr == "" {
+		cfg.db.maxIdleConns = DefaultMaxIdleConns
+	} else {
+		maxIdleConns, err := strconv.Atoi(maxIdleConnsStr)
+		if err != nil {
+			fmt.Printf("error loading max idle connections: %v \n", err)
+			os.Exit(1)
+		}
+		cfg.db.maxIdleConns = maxIdleConns
+	}
+
+	maxIdleTimeStr := os.Getenv("MAX_IDLE_TIME")
+	if maxIdleTimeStr == "" {
+		cfg.db.maxIdleTime = time.Duration(DefaultMaxIdleTime) * time.Minute
+	} else {
+		maxIdleTime, err := strconv.Atoi(maxIdleTimeStr)
+		if err != nil {
+			fmt.Printf("error loading max idle time: %v \n", err)
+			os.Exit(1)
+		}
+		cfg.db.maxIdleTime = time.Duration(maxIdleTime) * time.Minute
+	}
 }
